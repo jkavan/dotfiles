041692d40e1d86e48f1f0c89d6ae7860f1af745b
jkavan <jani@kavander.fi>
2023-08-24 12:50:52 +0300

    Disable yanking to clipboard

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,8 +2,13 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Map leader key to comma instead of space
 vim.g.mapleader = ","
 
 local opt = vim.opt
 
+-- Disable autowrite
 opt.autowrite = false
+
+-- Disable yanking to clipboard
+opt.clipboard = nil
